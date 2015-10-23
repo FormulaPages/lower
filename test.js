742d484b11011434aee600f6d1ec5215d648adb8
Peter Moresi <peter.moresi@gmail.com>
2015-10-22 20:46:44 -0700

    Fix test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var test = require('tape');
 var lower = require('./LOWER');
 
-test('it should lower case a string', (t) => {
+test('it should lower case a string', function(t) {
   t.plan(1)
   t.equal( lower("UPPER"), "upper" )
 })
